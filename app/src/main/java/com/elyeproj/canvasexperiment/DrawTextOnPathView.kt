6dd30f0858ede1a1d87f2f02d12b89276e7dd07b
elye <elye.project@gmail.com>
2019-04-03 20:47:12 +1100

    Draw TextOnPath

--- app/src/main/java/com/elyeproj/canvasexperiment/DrawTextOnPathView.kt
+++ app/src/main/java/com/elyeproj/canvasexperiment/DrawTextOnPathView.kt
@@ -7,7 +7,7 @@ import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
-class DrawTextPathView @JvmOverloads constructor(
+class DrawTextOnPathView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
