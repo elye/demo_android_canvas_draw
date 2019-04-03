1aa1391f4f6a94493bf08fa21d522a312b38c0f8
elye <elye.project@gmail.com>
2019-04-03 23:01:01 +1100

    Add drawBitmap, drawRGB and drawARGB

--- app/src/main/java/com/elyeproj/canvasexperiment/DrawBitmapView.kt
+++ app/src/main/java/com/elyeproj/canvasexperiment/DrawBitmapView.kt
@@ -1,24 +1,31 @@
 package com.elyeproj.canvasexperiment
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
-class DrawPointView @JvmOverloads constructor(
+class DrawBitmapView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
+    private val bitmap by lazy {
+        BitmapFactory.decodeResource(resources, R.drawable.image)
+    }
+
+    private val rect by lazy {
+        Rect(0 + paddingLeft, 0 + paddingTop, width - paddingRight, height - paddingBottom)
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (width == 0 || height == 0) return
-        canvas.drawPoint(
-            (width - paddingRight).toFloat(),
-            (height - paddingBottom).toFloat(), projectResources.paintBig
-        )
+
+        canvas.drawBitmap(bitmap, null, rect, null)
     }
 }
--- app/src/main/java/com/elyeproj/canvasexperiment/DrawRGBView.kt
+++ app/src/main/java/com/elyeproj/canvasexperiment/DrawRGBView.kt
@@ -2,23 +2,20 @@ package com.elyeproj.canvasexperiment
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
-class DrawPointView @JvmOverloads constructor(
+class DrawRGBView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (width == 0 || height == 0) return
-        canvas.drawPoint(
-            (width - paddingRight).toFloat(),
-            (height - paddingBottom).toFloat(), projectResources.paintBig
-        )
+        canvas.drawRGB(255, 0 , 0)
     }
 }
